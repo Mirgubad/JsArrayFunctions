c8e9e5e3a86f6807022a79a654e1e87b857f9488
Mirgubad <migubad@code.edu.az>
2022-08-12 13:40:31 +0400

    updated

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
 //This is The Custom Push Function of array
-//That receives An array ,Element and index then adding element this index which you given
+
 
 const array1=[1,3,5,7,9]
 
-CustomPush(array1,`Hello`,4)
-
-
+CustomPush(array1,`Hello`,5)
 
 function CustomPush(array,element,index){
+   if(index>array.length){
+    console.log(`ArrayIndexOutOfBounds`)
+    return;
+   }
+   
     const newarr=array.splice(index)
     array.push(element)
     const result=array.concat(newarr)
@@ -18,15 +21,19 @@ function CustomPush(array,element,index){
 
 
 
-//This Function is Receives An Array and index then Deleting item at this index
+//This Function is CustomPop
 
 const array2=[2,4,6,8,0]
 
-DeleteItemWithIndex(array2,3)
+CustomPop(array2,9)
 
-
-function DeleteItemWithIndex(array,deleteindex){
-    for(let i=0;i<array.length;i++){
+console.log(array2)
+function CustomPop(array,deleteindex){
+   if(deleteindex>array.length-1){
+    console.log(`ArrayIndexOutOfBounds`)
+    return;
+   }
+    for(let i=deleteindex;i<array.length;i++){
     
         if(i==deleteindex){
             array[i]=array[i+1]
